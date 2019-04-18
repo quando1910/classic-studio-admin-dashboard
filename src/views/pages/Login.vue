2f0807772efaa11bb20b5ea63070aff461d76d45
hoang.nguyen2 <hoang.nguyen2@asiantech.vn>
2019-04-18 08:59:38 +0700

    update

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -10,12 +10,30 @@
                   <h1>Login</h1>
                   <p class="text-muted">Sign In to your account</p>
                   <b-input-group class="mb-3">
-                    <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input type="text" class="form-control" placeholder="Username" autocomplete="username email" />
+                    <b-input-group-prepend>
+                      <b-input-group-text>
+                        <i class="icon-user"></i>
+                      </b-input-group-text>
+                    </b-input-group-prepend>
+                    <b-form-input
+                      type="text"
+                      class="form-control"
+                      placeholder="Username"
+                      autocomplete="username email"
+                    />
                   </b-input-group>
                   <b-input-group class="mb-4">
-                    <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input type="password" class="form-control" placeholder="Password" autocomplete="current-password" />
+                    <b-input-group-prepend>
+                      <b-input-group-text>
+                        <i class="icon-lock"></i>
+                      </b-input-group-text>
+                    </b-input-group-prepend>
+                    <b-form-input
+                      type="password"
+                      class="form-control"
+                      placeholder="Password"
+                      autocomplete="current-password"
+                    />
                   </b-input-group>
                   <b-row>
                     <b-col cols="6">
@@ -46,6 +64,6 @@
 
 <script>
 export default {
-  name: 'Login'
-}
+  name: "Login"
+};
 </script>
